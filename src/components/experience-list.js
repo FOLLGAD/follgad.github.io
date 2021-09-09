e7c0d8557ae022024ef5c78604e4dfc1b6bde119
Emil Ahlbäck <emil@tentium.se>
2021-09-09 12:26:21 +0200

    smaller headers

--- src/components/experience-list.js
+++ src/components/experience-list.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const ExperienceList = ({ children, name }) => (
 	<div className="mb-6 mt-4">
-		<h2 className="text-base uppercase tracking-widest font-bold text-blue-500">{name}</h2>
+		<h2 className="text-sm uppercase tracking-widest font-bold text-blue-500">{name}</h2>
 		{children}
 	</div>
 )
