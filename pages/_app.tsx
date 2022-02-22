450b4ef36e1a7b90dc1e74d1479a1b44db5d7e19
Emil Ahlbäck <emil.ahlback99@gmail.com>
2022-02-22 20:30:19 +0000

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import "@coreui/icons/css/all.min.css"
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
         return <>
@@ -10,16 +11,12 @@ function MyApp({ Component, pageProps }: AppProps) {
                         strategy="afterInteractive"
                 />
                 <Script id="google-analytics" strategy="afterInteractive">
-                        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-QF6T58D0LR');
-        `}
+                        {`window.dataLayer = window.dataLayer || [];function gtag(){window.dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-QF6T58D0LR');`}
                 </Script>
                 <Script defer data-domain="follgad.github.com" src="https://plausible.io/js/plausible.js" />
-
+                <Head>
+                  <title>Emil Ahlbäck</title>
+                </Head>
 
                 <Component {...pageProps} /></>
 }
