8b2d82a4c4804a520bb5f046ec01b71cda9fccf3
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2023-09-05 00:59:12 +0800

    add canonical site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,32 @@
-import '../styles/globals.css'
-import "@coreui/icons/css/all.min.css"
-import type { AppProps } from 'next/app'
-import Script from 'next/script'
-import Head from 'next/head'
+import "../styles/globals.css";
+import "@coreui/icons/css/all.min.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-        return <>
-                <Script
-                        src="https://www.googletagmanager.com/gtag/js?id=G-QF6T58D0LR"
-                        strategy="afterInteractive"
-                />
-                <Script id="google-analytics" strategy="afterInteractive">
-                        {`window.dataLayer = window.dataLayer || [];function gtag(){window.dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-QF6T58D0LR');`}
-                </Script>
-                <Script defer data-domain="follgad.github.com" src="https://plausible.io/js/plausible.js" />
-                <Head>
-                  <title>Emil Ahlbäck</title>
-                </Head>
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QF6T58D0LR"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];function gtag(){window.dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-QF6T58D0LR');`}
+      </Script>
+      <Script
+        defer
+        data-domain="follgad.github.com"
+        src="https://plausible.io/js/plausible.js"
+      />
+      <Head>
+        <title>Emil Ahlbäck</title>
+        <link rel="canonical" href="https://emil.zip" />
+      </Head>
 
-                <Component {...pageProps} /></>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
