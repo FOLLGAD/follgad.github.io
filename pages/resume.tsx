6e1ad2c45b2cb64b0e208364d13d3e4665dbed8d
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-02-11 21:24:34 +0000

    single col format

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -5,65 +5,54 @@ import Info from "../components/info";
 import Sublist from "../components/sublist";
 import SublistItem from "../components/sublist-item";
 
+const ContactInfo = () => (
+  <div className="mt-4 mb-3 ml-4 float-right rounded-lg p-4 pl-6 shadow-lg text-gray-700 bg-gray-100">
+    <h2 className="text-sm uppercase tracking-widest font-bold text-blue-500">
+      Contact me
+    </h2>
+
+    <Info icon="cil-location-pin">Stockholm, Sweden</Info>
+    <Info icon="cil-phone">+46 70 491 17 76</Info>
+    <a href="mailto:ahlback.emil@gmail.com">
+      <Info icon="cil-envelope-closed">emil@neostack.io</Info>
+    </a>
+    <a href="https://github.com/follgad">
+      <Info icon="cib-github">github.com/follgad</Info>
+    </a>
+    <a href="https://linkedin.com/in/emil-ahlback">
+      <Info icon="cib-linkedin">linkedin.com/in/emil-ahlback</Info>
+    </a>
+    <a href="https://twitter.com/emilahlback">
+      <Info icon="x">@emilahlback</Info>
+    </a>
+  </div>
+);
+
 const IndexPage = () => (
   <Layout>
-    <div className="flex">
-      <div className="flex-1 px-4">
-        <div className="text-4xl font-medium text-gray-700 mb-6 mt-3 inline-block">
-          Emil Ahlbäck
-        </div>
-
-        <div className="mb-3 ml-4 float-right rounded-lg p-4 pl-6 shadow-lg text-gray-700 bg-gray-100">
-          <h2 className="text-sm uppercase tracking-widest font-bold text-blue-500">
-            Contact me
-          </h2>
-
-          <Info icon="cil-location-pin">Stockholm, Sweden</Info>
-          <Info icon="cil-phone">+46 70 491 17 76</Info>
-          <a href="mailto:ahlback.emil@gmail.com">
-            <Info icon="cil-envelope-closed">emil@neostack.io</Info>
-          </a>
-          <a href="https://github.com/follgad">
-            <Info icon="cib-github">github.com/follgad</Info>
-          </a>
-          <a href="https://linkedin.com/in/emil-ahlback">
-            <Info icon="cib-linkedin">linkedin.com/in/emil-ahlback</Info>
-          </a>
-          <a href="https://twitter.com/emilahlback">
-            <Info icon="x">@emilahlback</Info>
-          </a>
-        </div>
+    <div className="flex flex-wrap px-4">
+      <div className="flex-1" style={{ flexBasis: "30%" }}>
         <div>
-          <p className="mt-2">
-            My name is Emil, I am a developer with a passion for
-            entrepreneurship and problem solving in real world applications.
-          </p>
-
-          <p className="mt-2">
-            I have a wide background in Software Engineering and Machine
-            Learning. I have for companies ranging from small startups to large
-            corporations. I have also started and run my own companies.
-          </p>
-
-          <p className="mt-2">
-            Currently looking for: Roles in Engineering and Research within the
-            field of AI.
-          </p>
-
-          <p className="mt-2">
-            Outside of work I enjoy running, reading and climbing.
-          </p>
+          <ContactInfo />
         </div>
-      </div>
-    </div>
+        <div className="flex-1">
+          <div className="text-4xl font-medium text-gray-700 mb-6 mt-3 inline-block">
+            Emil Ahlbäck
+          </div>
+          <div>
+            <p>
+              My name is Emil, I'm a Software Engineer with skills in
+              full-stack, AI and entrepreneurship. Currently looking for roles
+              in AI.
+            </p>
+          </div>
+        </div>
 
-    <div className="flex flex-wrap">
-      <div className="flex-1 px-4" style={{ flexBasis: "30%" }}>
         <ExperienceList name="Experience">
           <Experience
             mainTitle="Software Engineer — Godmode"
             from="Mar 2022"
-            to="2023"
+            to="Jan 2024"
             tags={["Nest.js", "Next.js", "React", "GCP", "Typscript"]}
           >
             <p>
@@ -78,6 +67,32 @@ const IndexPage = () => (
             </p>
           </Experience>
           <Experience
+            mainTitle="Godmode ⚡️"
+            link="https://godmode.space"
+            tags={["OpenAI", "GPT-4", "React", "Python"]}
+          >
+            <p>
+              I developed the first ever web-based platform for AI Agents
+              <span className="text-blue-600 noprint">
+                <a
+                  href="https://twitter.com/_Lonis_/status/1646641412182536196"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  [twitter post]
+                </a>
+              </span>
+              .
+            </p>
+            <p>
+              <strong>1M+</strong> users in the first 3 months.
+            </p>
+            <p>
+              <strong>500k+</strong> agents created.
+            </p>
+          </Experience>
+          <Experience
             mainTitle="Software Consultant — Self employed"
             from="Oct 2021"
             location="Neostack AB"
@@ -88,7 +103,7 @@ const IndexPage = () => (
               corporations. I also joined the Toptal network in 2021, an
               exclusive network of the top 3% of freelance developers.
             </p>
-            <p className="mt-2 italic">My clients include:</p>
+            <p className="mt-2">My clients include:</p>
             <ul className="list-disc pl-4">
               <li>
                 Signet Jewelers, the world's largest online jewelry retailer (AI
@@ -110,8 +125,8 @@ const IndexPage = () => (
             <p>I was one of two members of the frontend team at Depict.</p>
             <Sublist>
               <SublistItem>
-                I pushed code daily that reached millions of users through
-                e-commerce websites based in Sweden and worldwide.
+                I pushed code daily that reached <b>millions</b> of users
+                through e-commerce websites based in Sweden and worldwide.
               </SublistItem>
               <SublistItem>
                 Designed and developed their email recommendation system from
@@ -125,7 +140,44 @@ const IndexPage = () => (
                 Improved DX by automating the deployment process, helping
                 transition the entire codebase to Typescript, and creating a
                 library allowing compile-time JSX to allow writing fast and
-                efficient React-style code.
+                efficient React-style code
+                <a
+                  className="text-blue-600 noprint"
+                  href="https://www.npmjs.com/package/jsx2js"
+                >
+                  {" "}
+                  [npm]
+                </a>
+                .
+              </SublistItem>
+            </Sublist>
+          </Experience>
+          <Experience
+            mainTitle="Redditors Studios"
+            link="https://www.youtube.com/channel/UCmu_67Qpj7xBn7WxUoRi8FQ"
+            subTitle="Apr 2019"
+            tags={["React", "Redux", "Node.js", "ffmpeg", "AWS"]}
+          >
+            <p>Redditors is a Youtube channel I started and ran.</p>
+            <Sublist>
+              <SublistItem>
+                I{" "}
+                <a
+                  className="text-blue-600"
+                  href="https://github.com/FOLLGAD/reddit-video-maker"
+                >
+                  developed and open-sourced a service
+                </a>{" "}
+                to automate video editing.
+              </SublistItem>
+              <SublistItem>
+                Cut down the time required by <strong>90%</strong>{" "}
+                (&lt;15min/video) while simultaneously improving the viewer
+                experience.
+              </SublistItem>
+              <SublistItem>
+                Reached <strong>100k</strong> subscribers and{" "}
+                <strong>40 million</strong> views in under a year of starting.
               </SublistItem>
             </Sublist>
           </Experience>
@@ -197,8 +249,7 @@ const IndexPage = () => (
             </p>
           </Experience>
         </ExperienceList>
-      </div>
-      <div className="flex-1 px-2">
+
         <ExperienceList name="Education">
           <Experience
             mainTitle="Artificial Intelligence, Nanyang Technological University"
@@ -207,89 +258,11 @@ const IndexPage = () => (
             to="2023"
           />
           <Experience
-            mainTitle="Computer Science, Royal Institute of Technology"
+            mainTitle="Computer Science, KTH Royal Institute of Technology"
             location="Stockholm, Sweden"
             from="2018"
             to="2023"
           />
-          <Experience
-            mainTitle="Technology Programme, ABB Industrigymnasium"
-            location="Västerås, Sweden"
-            from="2015"
-            to="2018"
-          />
-        </ExperienceList>
-        <ExperienceList name="Projects">
-          {/* <p className="mt-2 text-gray-900">I've done many projects just for fun:</p> */}
-          <Experience
-            mainTitle="Godmode ⚡️"
-            link="https://godmode.space"
-            tags={["OpenAI", "GPT-4", "React", "Python"]}
-          >
-            <p>
-              I developed the first web-based platform for AI Agents{" "}
-              <span className="text-blue-600">
-                <a
-                  href="https://twitter.com/_Lonis_/status/1646641412182536196"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  [twitter post]
-                </a>
-              </span>
-              .
-            </p>
-            <p>
-              <strong>1M+</strong> users in the first 3 months.{" "}
-              <strong>500k+</strong> agents created.
-            </p>
-          </Experience>
-          <Experience
-            mainTitle="Redditors Studios"
-            link="https://www.youtube.com/channel/UCmu_67Qpj7xBn7WxUoRi8FQ"
-            subTitle="Apr 2019"
-            tags={["React", "Redux", "Node.js", "ffmpeg", "AWS"]}
-          >
-            <p>Redditors is a Youtube channel I started and ran.</p>
-            <Sublist>
-              <SublistItem>
-                I{" "}
-                <a
-                  className="text-blue-600"
-                  href="https://github.com/FOLLGAD/reddit-video-maker"
-                >
-                  developed and open-sourced a service
-                </a>{" "}
-                to automate video editing.
-              </SublistItem>
-              <SublistItem>
-                Cut down the time required by <strong>90%</strong>{" "}
-                (&lt;15min/video) while simultaneously improving the viewer
-                experience.
-              </SublistItem>
-              <SublistItem>
-                Reached <strong>100k</strong> subscribers and{" "}
-                <strong>40 million</strong> views in under a year of starting.
-              </SublistItem>
-            </Sublist>
-          </Experience>
-        </ExperienceList>
-        <ExperienceList name="Skills">
-          <Experience mainTitle="Javascript">
-            I have many years of experience using the Javascript/Node.js
-            ecosystem
-          </Experience>
-          <Experience mainTitle="Python">
-            I have used Python for many years, both for web and ML.
-          </Experience>
-          <Experience mainTitle="React">
-            My stack of choice is React and Node.js
-          </Experience>
-        </ExperienceList>
-        <ExperienceList name="Languages">
-          <Experience mainTitle="Swedish" subTitle="Native fluency" />
-          <Experience mainTitle="English" subTitle="Full fluency" />
-          <Experience mainTitle="Spanish" subTitle="Intermediate (learning)" />
         </ExperienceList>
       </div>
     </div>
