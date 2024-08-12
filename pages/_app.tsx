1450f23922e3234cca32ed8278a3c4e62a571498
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-08-13 01:10:37 +0200

    remove plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,11 +26,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Script id="google-analytics" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];function gtag(){window.dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-QF6T58D0LR');`}
       </Script>
-      <Script
-        defer
-        data-domain="emil.zip"
-        src="https://plausible.io/js/script.tagged-events.outbound-links.js"
-      />
       <Head>
         <title>Emil Ahlbäck</title>
         <link rel="canonical" href="https://emil.zip" />
